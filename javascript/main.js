0258b22d03a6137a76128c84f87b690c4fb8eb8e
ofece <ofeceevaristo@gmail.com>
2025-06-13 17:21:05 -0300

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+var backImage = document.querySelector('#landingMainText')
+
+var list = ["primeira", "segunda", "terceira", "quarta", "quinta"]
+backImage.classList.transition = "ease-in"
+
+var i = 0
+function changeBackground() {
+    backImage.classList.add(list[i])
+    if(i > list.length - 2) {
+        for (let j = 0; j < list.length; j++) {
+            backImage.classList.remove(list[j])
+        }
+        i = 0
+        changeBackground()
+    } else{
+        i++
+    }
+}
+
+changeBackground()
+
+setInterval(() => {
+    changeBackground();
+}, 5500);
