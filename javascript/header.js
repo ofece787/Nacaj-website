3992bce141c6aeb24d122258c5caa7db19c80363
ofece787 <ofeceevaristo@gmail.com>
2025-06-27 13:49:40 -0300

    landing page main section working on

--- javascript/header.js
+++ javascript/header.js
@@ -1,7 +1,10 @@
 var navList = document.querySelector('#nav-list')
 var menuButton = document.querySelector('#menu')
+var seta = document.querySelector('#seta-para-baixo')
 
 window.addEventListener('scroll', retract)
+seta.addEventListener('click', landingScrollDown)
+
 function retract() {
     getState = navList.classList[0]
 
@@ -23,4 +26,11 @@ function menuExtend() {
         navList.classList.add('retracted')
     }
     
+}
+
+function landingScrollDown() {
+    window.scrollTo({
+        top: 500,
+        behavior: "smooth"
+    })
 }
