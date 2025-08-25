9c9e3259f89950caa26ca3a341543b0a365734d6
ofece787 <ofeceevaristo@gmail.com>
2025-08-25 12:50:24 +1200

    paginas de noticias ganha noticia de destaques na parte superior, uma banner que mostrar a imagem, o titulo e parte do conteudo da noticia

--- javascript/imgAct.js
+++ javascript/imgAct.js
@@ -1,4 +1,5 @@
 let activitiesImages = document.querySelectorAll('.img')
+let landingImagesDiv = document.querySelectorAll('.landingFirstImg')
 
 
 let images = [
@@ -8,10 +9,27 @@ let images = [
     '../imagens/1749555964931.jpg',
     '../imagens/1749556013214.jpg'
 ]
-let i = 0
-activitiesImages.forEach((element) => {
-    element.style.background = `url(${images[i]})`;
+
+let landingImages = [
+    'imagens/1749556145269.jpg',
+    'imagens/1749556390655.jpg',
+    'imagens/1749556013214.jpg', 
+    'imagens/1749556142687.jpg',
+    'imagens/1749555964931.jpg',
+    'imagens/1749556199825.jpg'
+]
+let imageIterator1 = 0
+activitiesImages.forEach((element, ) => {
+    element.style.background = `url(${images[imageIterator1]})`;
     element.style.backgroundSize = 'cover';
     element.style.backgroundPosition = 'center';
-    i++
+    imageIterator1++
+})
+
+let landingPageIterator = 0
+landingImagesDiv.forEach((element) => {
+    element.style.background = `url(${landingImages[landingPageIterator]})`
+    element.style.backgroundSize = 'cover';
+    element.style.backgroundPosition = 'center';
+    landingPageIterator++
 })
