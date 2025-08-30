ba8fb33a694ce9eb128294adf3359b140f40a0ec
ofece787 <ofeceevaristo@gmail.com>
2025-08-30 15:58:54 +1200

    trabalhando na landing page exatamente no cabecalho, efetivamente no painel de boas vindas, track dos backgrounds por uma barra pontilhada para identificar exatamente qual background e numericamente, remocao do blur do background para uma visao mais minimalista, adicao da aba de parceiros no rodape

--- javascript/main.js
+++ javascript/main.js
@@ -1,24 +1,38 @@
 var backImage = document.querySelector('#landingMainText')
 var seta = document.querySelector('.outline')
+let progress = document.querySelectorAll('.dots')
 seta.addEventListener('click', landingScrollDown)
 
 
-var list = ["primeira", "segunda", "terceira", "quarta", "quinta"]
+var list = [
+    'url(../imagens/primeira_img.jpg)',
+    'url(../imagens/1749555959361.jpg)',
+    'url(../imagens/1749556013214.jpg)',
+    'url(../imagens/1749556152476.jpg)',
+    'url(../imagens/1749556265346.jpg)'
+]
 backImage.classList.transition = "ease-in"
+backImage.style.backgroundPosition = 'center'
+backImage.style.backgroundSize = 'cover'
 
 //Este e o codigo responsavel por trocar as imagens de fundo da landing page
 //Ass imagens sao trocadas com base num cronometro de 5.5 segundos para que
 //cada imagem tenha um tempo de tela rasuavel.
 var i = 0
 function changeBackground() {
-    backImage.classList.add(list[i])
+    backImage.style.backgroundImage = `${list[i]}`
+    
     if(i > list.length - 1) {
+        progress.forEach((element) => {
+            element.classList.remove('atual')
+        })
         for (let j = 0; j < list.length; j++) {
-            backImage.classList.remove(list[j])
+            backImage.style.backgroundImage = `${list[i]}`
         }
         i = 0
         changeBackground()
     } else{
+        progress[i].classList.add('atual')
         i++
     }
 }
@@ -28,7 +42,7 @@ changeBackground()
 //Esta e a funcao embutidade na linguagem que usei para cronometrar a funcao de mudanca de fundo.
 setInterval(() => {
     changeBackground();
-}, 5500);
+}, 4000);
 
 //Este e a funcao responsavel pela accao da seta na landing page para rolar para baixo se o usuario
 //nao souber se existe algo mais abaixo, a funcao faz uma rolagem de 300 pixels.
